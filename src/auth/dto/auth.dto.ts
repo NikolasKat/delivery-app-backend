36d9535dfc9d9ae0b426b48abefdc9fe017873f6
unknown <nikakotov2005@gmail.com>
2025-06-29 00:27:18 +0300

    unimportant changes

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -3,6 +3,7 @@ import { IsEmail, IsString, MinLength } from "class-validator"
 export class AuthDto {
 	@IsEmail()
 	email: string
+
 	@IsString()
 	@MinLength(6, { message: "Password must be at least 6 characters long" })
 	password: string
